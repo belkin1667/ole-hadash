a0e49cfae3a3e1e62d23f791e240d9587ef904c6
Belkin Mike <bel.kin1667@gmail.com>
2023-12-05 14:09:16 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import LandingPage from './LandingPage'; // adjust the path according to your project structure
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
+    <div className="App" style={{ backgroundColor: '#F5F5F5', height: '100vh' }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LandingPage />
+      </LocalizationProvider>
     </div>
   );
 }
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
 
 function LandingPage() {
+
     const [dateRanges, setDateRanges] = useState([{ start: '', end: '' }]);
-    const [alyaDate, setAlyaDate] = useState('');
+    const [aliyahDate, setaliyahDate] = useState('');
+
 
     const updateDateRange = (index, startOrEnd, date) => {
         const newDateRanges = [...dateRanges];
@@ -15,7 +19,7 @@ function LandingPage() {
         dateRanges.forEach((range, index) => {
             fullyFilled += (range.start && range.end) ? 1 : 0;
         });
-        if (dateRanges.length == 0 || fullyFilled == dateRanges.length) {
+        if (dateRanges.length === 0 || fullyFilled === dateRanges.length) {
             setDateRanges([...dateRanges, { start: '', end: '' }]);
         }
     };
@@ -24,9 +28,7 @@ function LandingPage() {
         for (let i = 0; i < dateRanges.length - 1; i++) {
             const range = dateRanges[i];
             if (!range.start && !range.end) {
-                const newDateRanges = [...dateRanges];
-                newDateRanges.splice(i, 1);
-                setDateRanges(newDateRanges);
+                removeRange(i);
             }
         }
         let fullyFilled = 0;
@@ -34,17 +36,18 @@ function LandingPage() {
             fullyFilled += (range.start && range.end) ? 1 : 0;
         });
         if (!dateRanges[dateRanges.length - 1].start && !dateRanges[dateRanges.length - 1].end && fullyFilled < dateRanges.length - 1) {
-            const newDateRanges = [...dateRanges];
-            newDateRanges.splice(dateRanges.length - 1, 1);
-            setDateRanges(newDateRanges);
+            removeRange(dateRanges.length - 1);
         }
     };
 
-    useEffect(addRanges, [dateRanges]);
-    useEffect(removeRanges, [dateRanges]);
+    const removeRange = (index) => {
+        const newDateRanges = [...dateRanges];
+        newDateRanges.splice(index, 1);
+        setDateRanges(newDateRanges);
+    };
 
     const calculateDaysInside = (daysAbroad) => {
-        const totalDays = Math.ceil((Date.now() - new Date(alyaDate)) / 1000 / 60 / 60 / 24);
+        const totalDays = Math.ceil((Date.now() - new Date(aliyahDate)) / 1000 / 60 / 60 / 24);
         return totalDays - daysAbroad;
     };
 
@@ -59,33 +62,33 @@ function LandingPage() {
                 totalDays += diffDays;
             }
         });
-        return totalDays;
+        return totalDays + 1;
     };
 
     const calculateDarkonStats = () => {
-        if (!alyaDate) {
+        if (!aliyahDate) {
             return {};
         }
-        const alya = new Date(alyaDate);
-        const alyaPlusYear = new Date(alyaDate).setFullYear(alya.getFullYear() + 1);
+        const Aliyah = new Date(aliyahDate);
+        const AliyahPlusYear = new Date(aliyahDate).setFullYear(Aliyah.getFullYear() + 1);
         const daysAbroad = calculateDaysAbroad();
         const daysInside = calculateDaysInside(daysAbroad);
-        const totalDaysSinceAlya = daysAbroad + daysInside;
+        const totalDaysSinceAliyah = daysAbroad + daysInside;
         var freeDays = 0;
         var dateSinceDarkonAvailable;
-        const daysPeriod = Math.max(365, totalDaysSinceAlya);
+        const daysPeriod = Math.max(365, totalDaysSinceAliyah);
         const daysThreshold = Math.ceil(3 * daysPeriod / 4.0)
         if (daysInside >= daysThreshold) { // набрали 3/4 дней
             freeDays = Infinity;
-            dateSinceDarkonAvailable = maxDate(alyaPlusYear + 1, Date.now());
-        } else if (totalDaysSinceAlya < daysPeriod
-            && daysPeriod - totalDaysSinceAlya + daysInside >= daysThreshold) { // еще можем набрать 3/4 дней 
-            freeDays = daysPeriod - daysThreshold - (totalDaysSinceAlya - daysInside);
-            dateSinceDarkonAvailable = alyaPlusYear + 1;
+            dateSinceDarkonAvailable = maxDate(AliyahPlusYear + 1, Date.now());
+        } else if (totalDaysSinceAliyah < daysPeriod
+            && daysPeriod - totalDaysSinceAliyah + daysInside >= daysThreshold) { // еще можем набрать 3/4 дней 
+            freeDays = daysPeriod - daysThreshold - (totalDaysSinceAliyah - daysInside);
+            dateSinceDarkonAvailable = AliyahPlusYear + 1;
         } else { // можем набрать 3/4 дней только компенсируя доп. днями
             freeDays = 0;
             const overspentDays = -(daysPeriod - daysThreshold - (daysPeriod - daysInside));
-            dateSinceDarkonAvailable = alyaPlusYear + 1 + overspentDays * 3;
+            dateSinceDarkonAvailable = AliyahPlusYear + 1 + overspentDays * 3;
         }
         return {
             daysAbroad: daysAbroad,
@@ -94,6 +97,7 @@ function LandingPage() {
             dateSinceDarkonAvailable: dateSinceDarkonAvailable
         };
     }
+
 
     const maxDate = (date1, date2) => {
         return date1 > date2 ? date1 : date2;
@@ -115,39 +119,50 @@ function LandingPage() {
         return [day, month, year].join('.');
     }
 
+    const inputDatesComponent = () => {
+        return (
+            <div>
+                <h1>5 year Darkon calculator</h1>
+                <h2>Aliyah Date</h2>
+                <label>
+                    <DatePicker
+                        value={aliyahDate}
+                        onChange={e => setaliyahDate(e.target.value)}
+                    />
+                </label>
+                <h2>Days Abroad</h2>
+                {dateRanges.map((range, index) => (
+                    <div key={index}>
+                        <DatePicker
+                            value={range.start || ''}
+                            onChange={e => updateDateRange(index, 'start', e)}
+                            maxDate={range.end || ''}
+                        />
+                        <DatePicker
+                            value={range.end || ''}
+                            onChange={e => updateDateRange(index, 'end', e)}
+                            minDate={range.start || ''}
+                        />
+                    </div>
+                ))}
+                <Button color="success" size="large" variant="contained">Calculate</Button>
+            </div>
+        );
+    }
+
     return (
         <div>
-            <h1>5 year Darkon calculator</h1>
-            <h2>Alya Date</h2>
-            <label>
-                <input
-                    type="date"
-                    value={alyaDate}
-                    onChange={e => setAlyaDate(e.target.value)}
-                />
-            </label>
-            <h2>Days Abroad</h2>
-            {dateRanges.map((range, index) => (
-                <div key={index}>
-                    <input
-                        type="date"
-                        value={range.start || ''}
-                        onChange={e => updateDateRange(index, 'start', e.target.value)}
-                        max={range.end}
-                    />
-                    <input
-                        type="date"
-                        value={range.end || ''}
-                        onChange={e => updateDateRange(index, 'end', e.target.value)}
-                        min={range.start}
-                    />
-                </div>
-            ))}
-            <h3>Total days abroad: {calculateDarkonStats().daysAbroad}</h3>
-            <h3>Total days inside: {calculateDarkonStats().daysInside}</h3>
-            <h3>Days abroad left: {calculateDarkonStats().freeDays}</h3>
-            <h3>Date since 5 year Darkon available: {formatDate(calculateDarkonStats().dateSinceDarkonAvailable)}</h3>
-        </div>
+            <h1>Darkon Calculator</h1>
+            <div>
+                <div>To get Darkon Passport for 5 years you have to spend in Israel at least 75% of your time since your Aliyah date</div>
+                <div>This tool calculates when you can get your passport and how may days you have abroad to plan your vacations and business trips carefully</div>
+            </div>
+            <Button color="success" size="large" variant="contained">Buy me a coffee!</Button>
+            <div>
+                {inputDatesComponent()}
+                <img src={require('./Darkon.png')} alt="Static Image" style={{ padding: '0 200px 0 0', width: '500px' }} />
+            </div>
+        </div >
     );
 }
 
